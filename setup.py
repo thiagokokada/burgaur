7ea8ee9f73c013536342dab4a2fadf049a295029
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2015-03-05 17:02:46 -0300

    setup.py: reduce duplication of information

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
+import os
 from setuptools import setup
+from imp import load_source
+
+burgaur = load_source("burgaur", "burgaur")
+
+def read(fname):
+    filename = os.path.join(os.path.dirname(__file__), fname)
+    return open(filename).read().replace('#', '')
 
 setup(
     name="burgaur",
-    version="0.1",
-    author="Thiago Kenji Okada",
-    author_email="thiago.mast3r@gmail.com",
+    version=burgaur.__version__,
+    author=burgaur.__author__,
+    author_email=burgaur.__email__,
+    maintainer=burgaur.__maintainer__,
+    maintainer_email=burgaur.__email__,
     description=("A delicious AUR helper. Made from cower."),
-    license="MIT",
+    license=burgaur.__license__,
     url="https://github.com/m45t3r/burgaur",
     scripts=["burgaur"],
+    platforms=["Linux"],
+    long_description=read("README.rst"),
 )
